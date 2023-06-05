920cae95721dfbce458e6bc1ad31f63022ab7de0
Zahra <88595482+zahrashabani2000@users.noreply.github.com>
2023-06-05 11:36:47 +0330

    Update convert.py

--- iot/convert.py
+++ iot/convert.py
@@ -1,12 +1,12 @@
 import csv
 import sqlite3
 
-conn = sqlite3.connect('E:\Django_Book_Projects\IOT\iot\db.sqlite3')
+conn = sqlite3.connect('db.sqlite3')
 cursor = conn.cursor()
 cursor.execute("select * from data_scan;")
 print("Successfully Connected to SQLite")
 
-with open("E:\Django_Book_Projects\IOT\iot\out.csv", 'w',newline='') as csv_file: 
+with open("out.csv", 'w',newline='') as csv_file: 
     csv_writer = csv.writer(csv_file)
     csv_writer.writerow([i[0] for i in cursor.description]) 
     csv_writer.writerows(cursor)
