fade3e59afdb8f7cc86c7e959b65afafe59a1877
Vision <vanda.vision@aol.com>
2023-06-05 13:08:14 +0430

    Endpoint Admin Scan

--- iot/data/admin.py
+++ iot/data/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 
 from data import models
 
-
-admin.site.register(models.Scan)
+@admin.register(models.Scan)
+class ScanAdmin(admin.ModelAdmin):
+    list_display = ('id', 'uuid', 'time_register')
+    search_fields = ['uuid', 'time_register']
+    list_filter = ('uuid', 'time_register')
